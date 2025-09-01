d35d9d13367ecda081bcf55f6a695471f52abe7c
ko_haru_58 <3muro87@gmail.com>
2025-09-01 16:42:32 +0900

    index.html 修正2回目

--- js/main.js
+++ js/main.js
@@ -17,36 +17,3 @@ $(function(){
         $('html, body').removeClass('u-no-scroll');
     })
 });
-
-/***************************************************************************** 
-「Take Out」「Eat In」内のテキストの文章に合わせてボックスの高さを変動させる
-******************************************************************************/
-function changeBoxHeights() {
-    //テキストの文章を囲む領域のサイズの調整
-    const boxes = document.querySelectorAll(".c-service-card__text-container");
-
-    boxes.forEach(box => box.style.height = "auto");    // 一旦リセット
-
-    let maxHeight = 0;
-    boxes.forEach(box => {
-        maxHeight = Math.max(maxHeight, box.offsetHeight);
-    });
-
-    boxes.forEach(box => {
-        box.style.height = maxHeight + "px";
-    });
-
-    //背景の画像のサイズの調整
-    //const wrappers = document.querySelectorAll(".c-service-card__wrapper");
-    //const imgs = document.querySelectorAll(".c-service-card__image");
-
-    //imgs.forEach(img => img.style.height = "auto");
-
-    //imgs.forEach((img, idx) => {
-    //    img.style.height = wrappers[idx].offsetHeight + "px";
-    //});
-}
-
-// 初期表示 & リサイズ時に実行
-window.addEventListener("load", changeBoxHeights);
-window.addEventListener("resize", changeBoxHeights);
